dba949dc4d9c808a86637ce94080a50f0855768a
1046099511 <1046099511@qq.com>
2024-03-07 11:03:54 +0800

    feat: 写个简单的demo，有空继续写

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-import { window } from 'vscode'
+import { statSync, writeFileSync } from 'node:fs'
+import { RelativePattern, window, workspace } from 'vscode'
 
 export function activate() {
-  window.showInformationMessage('Hello')
+  // const folders = workspace.workspaceFolders ?? []
+
+  workspace.onDidCreateFiles((e) => {
+    for (const file of e.files) {
+      if (statSync(file.fsPath)?.isFile?.() && file.fsPath.endsWith('.js')) {
+        writeFileSync(
+          file.fsPath,
+          '// create by mowtwo',
+          { encoding: 'utf-8' },
+        )
+      }
+    }
+  })
 }
 
 export function deactivate() {
